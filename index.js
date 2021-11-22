b8ad2a0907ff8efc7bd65008eb06501d18e1daf1
Tim Quimbo <tquimbo1@gmail.com>
2021-11-21 21:26:32 -0500

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,7 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
-
-let allWagesFor = function () {
+ let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -19,4 +18,60 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+let createEmployeeRecord = function(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(array){
+    return array.map(array => createEmployeeRecord(array));
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(" ");
+    let record = { 
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date,
+    };
+    this.timeInEvents.push(record);
+    return this;
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(" ");
+    let record = { 
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date,
+    };
+    this.timeOutEvents.push(record);
+    return this;
+}
+  
+let hoursWorkedOnDate = function(date){
+    let timeIn = this.timeInEvents.find(record => record.date === date);
+    let timeOut = this.timeOutEvents.find(record => record.date === date);
+    return (timeOut.hour - timeIn.hour)/100;
+}
+  
+let wagesEarnedOnDate = function(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+  
+let findEmployeeByFirstName = function(array, name){
+    return array.find(employee => employee.firstName === name);
+}
+  
+let calculatePayroll = function(employeeArray){
+    return employeeArray.reduce(function(totalPay, employee){
+        return totalPay + allWagesFor.call(employee)}, 0)
 }
